a32925f67ec45b8fbe7b29a5bad8fa4230b87ed3
elledcks <135715627+elledcks@users.noreply.github.com>
2024-03-06 08:45:23 -0300

    Add files via upload

--- ExercicioVariaveis.java
+++ ExercicioVariaveis.java
@@ -1,35 +1,27 @@
 public class ExercicioVariaveis {
     public static void main(String[] args) {
-        // Declaração e inicialização de variáveis de tipos primitivos
-        
-        // Tipos numéricos inteiros
-        byte byteVar = 10;
-        short shortVar = 1000;
-        int intVar = 100000;
-        long longVar = 1000000000L; // Utiliza L para indicar que é um literal long
-        
-        // Tipos numéricos de ponto flutuante
-        float floatVar = 3.14f; // Utiliza f para indicar que é um literal float
-        double doubleVar = 3.14159265359;
-        
-        // Tipo char
-        char charVar = 'A'; // Caractere simples
-        
-        // Tipo booleano
-        boolean booleanVar = true;
-        
-        // Impressão dos valores das variáveis
-        System.out.println("Valor da variável byteVar: " + byteVar);
-        System.out.println("Valor da variável shortVar: " + shortVar);
-        System.out.println("Valor da variável intVar: " + intVar);
-        System.out.println("Valor da variável longVar: " + longVar);
-        System.out.println("Valor da variável floatVar: " + floatVar);
-        System.out.println("Valor da variável doubleVar: " + doubleVar);
-        System.out.println("Valor da variável charVar: " + charVar);
-        System.out.println("Valor da variável booleanVar: " + booleanVar);
-        
-        // Utilização de caracteres especiais em strings
-        String specialString = "Esta é uma string com caracteres especiais: ç, á, é, í, ó, ú, ã, õ, â, ê, î, ô, û.";
-        System.out.println(specialString);
+        int inteiro = 10;
+        long longo = 100000000L;
+        float flutuante = 3.14f;
+        double duplo = 4.16759265359;
+        char caractere = 'D';
+        boolean booleano = true;
+        byte bytes = 127;
+        short curto = 32000;
+
+        System.out.println("Valor da variável inteiro " + inteiro);
+        System.out.println("Valor da variável longo " + longo);
+        System.out.println("Valor da variável flutuante" + flutuante);
+        System.out.println("Valor da variável duplo " + duplo);
+        System.out.println("Valor da variável caractere " + caractere);
+        System.out.println("Valor da variável booleano: " + booleano);
+        System.out.println("Valor da variável bytes " + bytes);
+        System.out.println("Valor da variável curto" + curto);
+
+        // Utilizando caracteres especiais
+        System.out.println("String com caractere de nova linha:\nTexto em nova linha");
+        System.out.println("String com caractere de tabulação:\tTexto com tabulação");
+        System.out.println("String com aspas duplas:\"Texto entre aspas\"");
+        System.out.println("String com barra invertida:\\Texto com barra invertida");
     }
 }
